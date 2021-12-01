aeb6b95b79029e427e1f430e9ebfd00740a2353b
Zane Geiser <57169901+ZaneGeiser@users.noreply.github.com>
2021-12-01 17:50:44 +1100

    "Users able to update username and email."
    "add feature to user profile form so that they can update their profile
    username, email, and profile picture. Also resize profile pic to 300 pixles
    and placed next to post on homepage. Still need to remove old profile pic
    from the server."

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+import os
 
 
 class Profile(models.Model):
@@ -8,3 +10,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    #Extends save method to downsize image
+    def save(self):
+        os.remove(self.user.profile.image.path)
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
