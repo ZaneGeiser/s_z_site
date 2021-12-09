977103a87812b4563bd236f24d1f92aab4d2fe0e
ZaneGeiser <zanegeiser@gmail.com>
2021-12-09 21:44:43 +1100

    added django-heroku

--- s_z_site/settings.py
+++ s_z_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # OLD: BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,3 +164,5 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
